7eae54a3d7049bf949126be076ca340f27b82a02
Your Name <zohaibabbas8557@gmail.com>
2025-05-31 08:32:43 +0500

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./components/ui/button";
+
+function App() {
+	return (
+		<>
+			<div className="">
+				<Button>Click Me</Button>
+			</div>
+		</>
+	);
+}
+
+export default App;
